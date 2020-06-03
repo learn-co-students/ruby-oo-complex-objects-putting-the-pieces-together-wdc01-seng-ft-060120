a77bb86ea2eb67ae7732ccfef10b433025d32f21
andy-aguilar <aaguil3@gmail.com>
2020-06-03 09:46:29 -0400

    done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,14 @@
 # Make your shoe class here!
 class Shoe
-    # attr_accessor :author, :page_count, :genre
-attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
     
     def initialize(brand)
         @brand = brand
     end
 
-    def turn_page
-        puts "Flipping the page...wow, you read fast!"
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
     end
 end
